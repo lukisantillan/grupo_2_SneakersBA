b8c6fb72f7f10fdfb028f7788c517705839afd58
Lucas Santillan <108704474+lukisantillan@users.noreply.github.com>
2022-10-20 18:44:05 -0300

    agrego estructura html a documento register y login, corrijo desperfectos del codigo general

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ app.listen(APP_PORT, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/index.html'));
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
+});
+
+app.post('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/home.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'));
+});
+
+app.get('/productCar', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/productCart.html'))
+});
