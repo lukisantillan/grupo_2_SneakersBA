2c3c8d39e82326212748794924e4f3f770dabea9
rucabrera <rubnc92@gmail.com>
2022-11-18 00:30:48 -0300

    se agregan rutas para crear producto,se modifica idioma

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,11 +1,48 @@
 var express = require('express');
 var router =  express.Router();
-const productController = require('../controllers/productController')
 
-router.get('/carrito', productController.carrito)
-router.get('/editar-producto', productController.editarProducto)
-router.get('/detalle/:id', productController.detail)
-router.get('/crear-producto', productController.crearProducto)
-router.get('/', productController.index);
+const productController = require('../controllers/productController')
+const path = require("path");
+const multer = require("multer");
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve("public/images"));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now();
+      const fileExtension = path.extname(file.originalname);
+      const newName = file.originalname.replace(fileExtension, "");
+      cb(null, newName + "-" + uniqueSuffix + fileExtension);
+    },
+  });
+  
+  const upload = multer({ storage });
+
+/*VISTA TODOS LOS PRODUCTOS */
+
+router.get("/", productController.index);
+
+/*CREAR*/
+router.get("/create",productController.createProduct);
+router.post(
+  "/",
+  upload.single("image"),
+  productController.store
+);
+
+/*VISUALIZAR UN PRODUCTO*/
+router.get("/detail/:productId/", productController.detail);
+
+/*EDITAR PRODUCTO*/
+router.get("/edit/:productId/", productController.edit);
+router.put("/:id", productController.update);
+
+/*BORRAR PRODUCTO/
+/*router.delete("/:id", productsController.destroy);*/
+
+router.get('/cart', productController.cart)
 
 module.exports= router;
