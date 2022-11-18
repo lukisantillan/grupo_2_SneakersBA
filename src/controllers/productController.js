61f5a9ad7ef5c7b022d518e30224e2506dbd7d9c
rucabrera <rubnc92@gmail.com>
2022-11-18 00:28:57 -0300

    se modifica controlador para crear producto, se unifica idioma

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,28 +3,89 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const { validationResult } = require('express-validator');
+
 const product = {
-    carrito: function (req, res, next) {
+    cart: function (req, res, next) {
         res.render('productCart')
     },
-    crearProducto: function (req, res, next) {
+    createProduct: function (req, res, next) {
         res.render('productCreate')
     },
-    detalleProdcuto: function (req, res, next) {
-        res.render('productDetail')
-    },
-    editarProducto: function (req, res, next) {
-        res.render('productEdit')
-    },
+    store: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render(
+        'productCreate',
+        {
+          errors: errors.array()
+        }
+      )
+
+    };
+    const camposDeNuevoProducto = req.body;
+    camposDeNuevoProducto.id = products.length;
+    camposDeNuevoProducto.image = req.file.filename;
+    products.push(camposDeNuevoProducto);
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+    return res.send(req.file);
+  },
+
     detail: (req, res) => {
-		let resultado = products.find(product => product.id == req.params.id)
-        res.render('productDetail', {'resultado' : resultado})
-	},
+        const productId = req.params.productId;
+        const productToFind = products.find((product) => product.id == productId);
+        if (productToFind == undefined) {
+          return res.send("No existe el producto");
+        }
+        return res.render("productDetail", { 'productToFind' : productToFind})
+      },
+    edit: (req, res) => {
+
+    const productId = req.params.productId;
+
+    const productToFind = products.find((product) => product.id == productId);
+
+    if (productToFind == undefined) {
+      return res.send("No existe el producto");
+    }
+    return res.render("productEdit", {
+		productToEdit: productToFind,
+    });
+  },
+
+  update: (req, res) => {
+    const dataToUpdate = req.body;
+    dataToUpdate.price = Number(dataToUpdate.price);
+    dataToUpdate.discount = Number(dataToUpdate.discount);
+
+    const productIndex = products.findIndex(
+      (product) => {
+        return product.id == req.params.id
+      }
+    )
+    if (productIndex == -1) {
+      return res.send('No existe el producto')
+    }
+    products[productIndex] = {
+      ...products[productIndex],
+      ...dataToUpdate
+    }
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+
+    return res.send(products[productIndex])
+  },
+
     index: (req, res) => {
-        const productsUrbanos = products.filter((product) => product.category =='Urbana')
-        const productsDeportivos = products.filter((product) => product.category == 'Deportiva')
+        const productsUrbanos = products.filter(product => product.category =='Urbana');
+        const productsDeportivos = products.filter(product => product.category == 'Deportiva');
         //Devolver datos a vista
-        return res.render('products', {productsUrbanos, productsDeportivos})
+        const viewData = {
+            productsUrbanos,
+            productsDeportivos,
+        }
+        return res.render('products', viewData)
         }
 };
 
