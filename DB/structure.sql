0e6ff4bbce3269f7ff514b9f05ff90f60f26b54b
rucabrera <rubnc92@gmail.com>
2023-01-18 20:25:02 -0300

    modifico structure

--- DB/structure.sql
+++ DB/structure.sql
@@ -16,29 +16,6 @@ CREATE TABLE `colour` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `orderdetails` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `OrderId` int(11) NOT NULL,
-  `ProductId` int(11) NOT NULL,
-  `Price` int(11) NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `OrderId` (`OrderId`),
-  KEY `ProductId` (`ProductId`),
-  CONSTRAINT `FK_orderdetails_order` FOREIGN KEY (`OrderId`) REFERENCES `orders` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_orderdetails_products` FOREIGN KEY (`ProductId`) REFERENCES `products` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `orders` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) NOT NULL,
-  `Amount` int(11) NOT NULL,
-  `ShippingAdress` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `FK_orders_users` (`UserId`),
-  CONSTRAINT `FK_orders_users` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 CREATE TABLE `products` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Sku` int(11) DEFAULT NULL,
@@ -59,6 +36,7 @@ CREATE TABLE `products` (
   CONSTRAINT `FK_products_colour` FOREIGN KEY (`ColourId`) REFERENCES `colour` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
 CREATE TABLE `users` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `FullName` varchar(100) NOT NULL,
@@ -67,4 +45,28 @@ CREATE TABLE `users` (
   `UserName` varchar(16) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `UserName` (`UserName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `orders` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `ShippingAdress` varchar(100) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_orders_users` (`UserId`),
+  CONSTRAINT `FK_orders_users` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `orderdetails` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `OrderId` int(11) NOT NULL,
+  `ProductId` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `OrderId` (`OrderId`),
+  KEY `ProductId` (`ProductId`),
+  CONSTRAINT `FK_orderdetails_order` FOREIGN KEY (`OrderId`) REFERENCES `orders` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_orderdetails_products` FOREIGN KEY (`ProductId`) REFERENCES `products` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
