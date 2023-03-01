86e93ce3bde09471ed31d3830f37b2e8d9d72079
Moro-Dev <matirodriguez428@gmail.com>
2023-03-01 11:22:42 -0300

    commit con el error en la logica del js de validaciones del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,49 @@
-const formulario = document.querySelector('#form-padre');
-const divErrores = document.querySelector('#errores');
-
-/*formulario.email.focus();*/
+window.addEventListener("load", function(){
 
 
-const procesarFormulario = (event) => {
-    event.preventDefault();
-
-    divErrores.innerHTML = '';
-
-
-    console.log(formulario)
-
-    const campoEmail = formulario.email;
-    const campoPassword = formulario.password;
-
-
-    console.log(campoPassword)
-
-    if (campoEmail.value == '') {
-        divErrores.innerHTML += '<li> El campo email está vacío </li>'
+    const formulario = document.querySelector('#form-padre');
+    const divErrores = document.querySelector('#errores');
+    
+    
+    formulario.email.focus()
+    
+    
+    const envioFormulario = (event) => {
+        event.preventDefault();
+    
+    
+        divErrores.innerHTML = '';
+    
+    
+    
+    
+        const campoEmail = formulario.email;
+        const campoPassword = formulario.password;
+    
+    
+    
+    
+        if (campoEmail.value == '') {
+            divErrores.innerHTML += '<li> El campo email está vacío </li>'
+        }
+    
+        if (campoPassword.value == '') {
+            divErrores.innerHTML += '<li> El campo password está vacío </li>'
+        }
+    
+    
+    
+    /*consulto si hay errores para asignar la clase al <ul> y hacer la lista visible con css*/ 
+    
+        if (divErrores.innerHTML !== "") {
+    
+            
+            divErrores.classList.add("conerrores");
+          }
+    
     }
-
-    if (campoPassword.value == '') {
-        divErrores.innerHTML += '<li> El campo password está vacío </li>'
-    }
-
-
-
-/*consulto si hay errores para asignar la clase al <ul> y hacer la lista visible con css*/ 
-
-    if (divErrores.innerHTML !== "") {
-
-        
-        divErrores.classList.add("conerrores");
-      }
-
-}
+    
+    formulario.addEventListener('submit', envioFormulario)
+    
+    
+    })
